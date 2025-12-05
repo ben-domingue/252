d59d1b995d44fef6a508d484cdb5cd972b65cc28
Ben Domingue <ben.domingue@gmail.com>
2025-12-05 12:45:56 -0800

    ps1

---
+++ ps1/ll_logreg.R
@@ -0,0 +1,13 @@
+b<- .5
+x<-rnorm(1000)
+p<-exp(b*x)/(1+exp(b*x))
+y<-rbinom(length(x),1,p)
+f<-function(b,x,y) {
+    p<-exp(b*x)/(1+exp(b*x))
+    sum((y-p)*x)
+}
+bs<-seq(-3,3,length.out=100)
+z<-numeric()
+for (i in 1:length(bs)) z[i]<-f(bs[i],x=x,y=y)
+plot(bs,z,xlab='candidate b',ylab='derivative',type='l',ylim=c(-1000,1000))
+
