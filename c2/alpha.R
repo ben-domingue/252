f822d43e4415446144a0bdc957c796e2240561f5
Ben Domingue <ben.domingue@gmail.com>
2026-01-13 11:08:36 -0800

    Update alpha.R

--- c2/alpha.R
+++ c2/alpha.R
@@ -13,7 +13,11 @@ psych::alpha(resp) ##what is the reliability estimate?
 ##Let's take two different perspectives on this:
 ## Item-item correlations
 cormat<-cor(resp)
-cormat[1:5,1:5] #consider items 1 and 4. 
+cormat[1:5,1:5] #consider items 1 (or 2) and 4.  responses are 0-6 with 6 being strongly agree
+#m01: Never tell anyone the real reason you did something unless it is useful to do so
+#m02: The best way to handle people is to tell them what they want to hear
+#m03: One should take action only when sure it is morally right
+#m04: Most people are basically good and kind
 
 ##A more conceptual perspective. https://cran.r-project.org/web/packages/lessR/refman/lessR.html#dataMach4
 psych::alpha(resp,check.keys=TRUE) ##much higher alpha but not necessarily right (we've automated the reverse coding but no guarantees....)
