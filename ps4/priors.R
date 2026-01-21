655d7d27bae9dc3f8a85773a58494c0f6de792c6
Ben Domingue <ben.domingue@gmail.com>
2026-01-20 18:12:20 -0800

    ps4

--- ps4/priors.R
+++ ps4/priors.R
@@ -2,8 +2,10 @@ df<-irw::irw_fetch('gilbert_meta_2')
 resp<-irw::irw_long2resp(df)
 resp$id<-NULL
 
+library(mirt)
 ni<-ncol(resp)
 s<-paste("F=1-",ni,"
-             PRIOR = (1-",ni,", a1, lnorm, 0.0, 1.0)",sep="")
+     PRIOR = (1-",ni,", a1, lnorm, 0.0, 1.0),(1-",ni,", g, expbeta, 2, 17)",sep="")
 model<-mirt.model(s)
-m1<-mirt(resp,model,itemtype=rep("2PL",ni),method="EM",technical=list(NCYCLES=2000))
+m1<-mirt(resp,model,itemtype=rep("3PL",ni),method="EM",technical=list(NCYCLES=5000))
+coef(m1,simplify=TRUE,IRTpars=TRUE)
