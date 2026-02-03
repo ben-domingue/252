b337b7b915044cf80dab30f574110f3c89577aef
Ben Domingue <ben.domingue@gmail.com>
2026-02-03 14:22:50 -0800

    ps7

---
+++ ps7/ilhte.R
@@ -0,0 +1,31 @@
+#df<-irw::irw_fetch("gilbert_meta_4")
+
+library(lme4)
+m<-lmer(resp~0+(1|id)+item+item:treat,df)
+fe<-fixef(m)
+tx<-grepl(":",names(fe))
+plot(fe[!tx],fe[tx],xlab='main item effects',ylab='treatment specific item effects',pch=19,cex=2)
+abline(h=mean(fe[tx]))
+
+df<-irw::irw_fetch("gilbert_meta_2")
+##getting post-test theta
+resp<-irw::irw_long2resp(df)
+m<-mirt::mirt(resp[,-1],1,'Rasch')
+th<-mirt::fscores(m)
+##putting data together
+z<-data.frame(id=resp[,1],th=th[,1])
+z2<-df[,c("id","std_baseline","treat")]
+z2<-z2[!duplicated(z2$id),]
+z<-merge(z,z2)
+##
+z$std_baseline<-as.numeric(z$std_baseline)
+##aggregate level
+m0<-lm(th~treat,z)
+m1<-lm(th~treat+std_baseline,z)
+m2<-lm(th~treat*std_baseline,z)
+##response level
+m2a<-lme4::glmer(resp~1+treat+std_baseline+(1|item)+(1|id),df,family='binomial')
+m3a<-lme4::glmer(resp~1+treat*std_baseline+(1|item)+(1|id),df,family='binomial')
+m4a<-lme4::glmer(resp~1+treat*std_baseline+(1+treat|item)+(1|id),df,family='binomial')
+
+lapply(list(m2a,m3a,m4a),function(x) summary(x)$coef)
