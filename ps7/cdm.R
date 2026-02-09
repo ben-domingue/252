d9d9841da3d6a3418a168cf7aab4080c8d68f98e
ben-domingue <ben.domingue@gmail.com>
2026-02-09 05:54:54 -0800

    ps7

--- ps7/cdm.R
+++ ps7/cdm.R
@@ -1,12 +1,12 @@
 df<-irw::irw_fetch("frac20")
 library(lme4)
+df$id<-paste('id',df$id,sep='') ##will help to track ids
 
 ##A
 ##lme4 style model
 m1<-lmer(resp~0+(1|id)+item,df)
 resp<-irw::irw_long2resp(df)
-resp$id<-NULL
-m1.irt<-mirt::mirt(resp,1,'Rasch')
+m1.irt<-mirt::mirt(resp[,-1],1,'Rasch')
 
 fe<-fixef(m1)
 names(fe)<-gsub("itemitem","item",names(fe))
@@ -23,24 +23,48 @@ m2<-lmer(as.formula(fm),df)
 loadings<-t(apply(df[df$id==170,nms],1,paste,collapse='')) ##170 is arbitrary
 table(loadings)
 
-
 ##C
 ##now a CDM approach
 library(GDINA)
-m3 <- GDINA(frac20$dat,frac20$Q,model="DINA") #you'll need to install GDINA
+##make qmatrix
+qm<-df[,grep("^Qm",names(df))]
+qm<-as.data.frame(qm)
+qm$item<-df$item
+qm<-qm[!duplicated(qm$item),]
+##ensure order is ok
+if (!all(qm$item==names(resp)[-1])) {
+    stop()
+} else {
+    qm$item<-NULL
+    print("all is well")
+}
 
+m3 <- GDINA(resp[,-1],qm,model="DINA") #you'll need to install GDINA
+
+##comparing
 coef(m3) #these show the probability of a correct response for a given pattern of skills. you can see the A part of DINA in the fact that the probabilities are constant for patterns that are not all 1s
 z<-personparm(m3) #here we have hard calls for whether a person has each of the 8 skills
 ##hard to know how to contrast theta estimates and skills but one question we can ask is about the relationship between skill acquisition and ability. 
 ##specifically let's look at ability estimates and average number of skills
-th<-ranef(m1)[[1]][,1]
-plot(th,rowSums(z)) 
+th.irt<-mirt::fscores(m1.irt)[,1]
+th.lmer<-ranef(m1)[[1]]
+person<-data.frame(id=resp$id,th=th.irt,sum.skill=rowSums(z))
+tmp<-data.frame(id=rownames(th.lmer),th.lmer=th.lmer[,1])
+person<-merge(person,tmp)
+
+plot(person[,-1])
+cor(person[,-1])
 
 ##now let's take another perspective: what if we modeled having a specific skill as a function of theta
-xl<-range(th)
+z<-personparm(m3)
+z<-data.frame(z)
+z$id<-resp$id
+z<-merge(z,person)
+xl<-range(person$th)
 plot(NULL,xlim=xl,ylim=0:1,xlab='theta',ylab='Pr(skill=1)')
-for (i in 1:ncol(z)) {
-    m<-glm(z[,i]~th,family='binomial')
+for (i in 1:8) {
+    fm<-paste("A",i,"~th",sep='')
+    m<-glm(formula(fm),z,family='binomial')
     xx<-seq(xl[1],xl[2],length.out=1000)
     co<-coef(m)
     yy<-1/(1+exp(-(co[1]+co[2]*xx)))
--- ps7/ilhte.R
+++ ps7/ilhte.R
@@ -1,8 +1,10 @@
 df<-irw::irw_fetch("gilbert_meta_2")
+
 ##getting post-test theta
 resp<-irw::irw_long2resp(df)
 m<-mirt::mirt(resp[,-1],1,'Rasch')
 th<-mirt::fscores(m)
+
 ##putting data together
 z<-data.frame(id=resp[,1],th=th[,1])
 z2<-df[,c("id","std_baseline","treat")]
@@ -10,14 +12,16 @@ z2<-z2[!duplicated(z2$id),]
 z<-merge(z,z2)
 ##
 z$std_baseline<-as.numeric(z$std_baseline)
-##aggregate level
-m0<-lm(th~treat,z)
+
+##aggregate level treatment effects [but no sum score version!]
+m0<-lm(th~treat,z) ##no prior test
 m1<-lm(th~treat+std_baseline,z)
 m2<-lm(th~treat*std_baseline,z)
-##response level
-m1a<-lme4::glmer(resp~1+treat+(1|item)+(1|id),df,family='binomial')
-m2a<-lme4::glmer(resp~1+treat+std_baseline+(1|item)+(1|id),df,family='binomial')
-m3a<-lme4::glmer(resp~1+treat*std_baseline+(1|item)+(1|id),df,family='binomial')
-m4a<-lme4::glmer(resp~1+treat*std_baseline+(1+treat|item)+(1|id),df,family='binomial')
 
-lapply(list(m2a,m3a,m4a),function(x) summary(x)$coef)
+##response level treatment effects
+##m0a<-lme4::glmer(resp~1+treat+(1|item)+(1|id),df,family='binomial') ##no prior test
+m1a<-lme4::glmer(resp~1+treat+std_baseline+(1|item)+(1|id),df,family='binomial')
+m2a<-lme4::glmer(resp~1+treat*std_baseline+(1|item)+(1|id),df,family='binomial')
+m3a<-lme4::glmer(resp~1+treat*std_baseline+(1+treat|item)+(1|id),df,family='binomial')
+
+lapply(list(m1a,m2a,m3a),function(x) summary(x)$coef)
