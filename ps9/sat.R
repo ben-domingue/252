27bef03999a32f32d0b8b38813d34ec139f4a4ea
ben-domingue <ben.domingue@gmail.com>
2026-02-23 05:47:24 -0800

    ps9

--- ps9/sat.R
+++ ps9/sat.R
@@ -1,36 +1,10 @@
-######################################
-##Block A
-hf<-irw::irw_fetch("imps2025_hf")
-table(hf$block,df$item)
-df<-hf[hf$block=="mixed test",]
-df$id<-paste(df$id,df$time) ##we are treating people at different times at different points
-##second definition [did shape change]
-L<-split(df,df$id)
-f<-function(x) {
-    z<-x[,c("trial_num","stim_shape")]
-    z$trial_num<-z$trial_num+1
-    names(z)[2]<-"last_shape"
-    y<-merge(x,z)
-    y
-}
-L<-lapply(L,f)
-df2<-data.frame(do.call("rbind",L))
-df2$switch<-ifelse(df2$last_shape!=df2$stim_shape,1,0)
-df<-df2
+library(lme4)
 
-df<-df[df$time_limit==1.25,]
-df<-df[df$rt<1,]
-m2<-glmer(resp~switch+item+(1|id),df2,family='binomial')
-df$lrt<-log(df$rt)
-df<-df[is.finite(df$lrt),]
-m2time<-glmer(resp~switch+item+(1|id)+lrt,df,family='binomial')
-
-
-######################################
-##Block B
+##ROAR
 df<-irw::irw_fetch("roar_lexical")
 df$lrt<-log(df$rt)
-m<-glmer(resp~(1|item)+(1|id)+lrt,df,family='binomial')
+roar<-df
+m.roar<-glmer(resp~(1|item)+(1|id)+lrt,roar,family='binomial')
 
 ##pisa
 df<-irw::irw_fetch("pisa2018_read")
@@ -38,8 +12,11 @@ df<-df[df$CNT=="ESP",] ##doing this from madrid!
 f<-function(x) length(unique(x[!is.na(x)]))
 nc<-by(df$resp,df$item,f)
 df<-df[df$item %in% names(nc)[nc==2],]
+##downsample
+ids<-sample(unique(df$id),2000)
+df<-df[df$id %in% ids,]
 df$lrt<-log(df$rt)
 df<-df[is.finite(df$lrt),]
-m<-lmer(resp~(1|item)+(1|id)+lrt,df)
+pisa<-df
+m.pisa<-glmer(resp~(1|item)+(1|id)+lrt,pisa,family='binomial')
 
-##maybe a good example but need to help them interpret this coefficient
